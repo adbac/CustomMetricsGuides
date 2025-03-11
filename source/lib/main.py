b56a401b24e841f2fa25ca75b7b0ec4f6fde3361
adbac <126618591+adbac@users.noreply.github.com>
2025-03-11 18:09:06 +0100

    first commit

---
+++ source/lib/main.py
@@ -0,0 +1,145 @@
+import math
+
+from fontTools.misc import transform
+from mojo.events import postEvent
+from mojo.subscriber import (Subscriber, listRegisteredSubscribers,
+                             registerGlyphEditorSubscriber,
+                             registerSubscriberEvent,
+                             unregisterGlyphEditorSubscriber)
+from mojo.UI import getDefault
+
+# originally written by Connor Davenport: https://gist.github.com/connordavenport/ea50758429974d12df3d5c114d9d491b
+
+
+
+border = getDefault("glyphViewVerticalPadding")
+fontsize = getDefault("textFontSize")
+
+metricsStrokeColor = getDefault("glyphViewFontMetricsStrokeColor")
+metricsTextColor = getDefault("glyphViewMetricsTitlesColor")
+
+KEY = "com.adbac.customMetricsGuides"
+
+'''
+start up script to draw custom metric guides in the
+glyph view using info stored in the font lib.
+'''
+
+
+class CustomMetricsGuidesSubscriber(Subscriber):
+
+    debug = True
+
+    def build(self):
+                
+        glyphEditor = self.getGlyphEditor()
+        self.container = glyphEditor.extensionContainer(
+            identifier=KEY,
+            location="background",
+            clear=True
+        )
+        self.glyph = glyphEditor.getGlyph()
+        self.leftMargin = self.glyph.leftMargin
+        self.rightMargin = self.glyph.rightMargin
+
+        self.merzMetrics = {}
+
+    def started(self):
+        self.drawCustomMetrics(self.glyph.font)
+        
+    def destroy(self):
+        self.container.clearSublayers()
+
+    def glyphEditorDidSetGlyph(self, info):
+        self.glyph = info["glyph"]
+        font = self.glyph.font
+        self.drawCustomMetrics(font)
+
+    def drawCustomMetrics(self, font):
+        # based on benedikt bramböck's `ShowVMetrics` tool
+        self.container.clearSublayers()
+
+        self.merzMetrics = {}
+        customMetrics = font.lib.get(KEY, {})
+        
+        for name, value in customMetrics.items():
+
+            displayName = f"{name} ({value})"
+            value = int(float(value))
+
+            # is there a better way to get the adjusted xPos??
+
+            # off = font.lib.get('com.typemytype.robofont.italicSlantOffset', 0)
+            # x = y = math.radians(-font.info.italicAngle or 0)
+            # matrix = transform.Identity.skew(x=x, y=y)
+            # t = transform.Transform()
+            # oX, oY = (0,0)
+            # t = t.translate(oX, oY)
+            # t = t.transform(matrix)
+            # t = t.translate(-oX, -oY)
+            # trans = tuple(t)
+            # ot = transform.Transform(*trans)
+            # n = ot.transformPoint((self.glyph.width + off, value))
+
+            self.merzMetrics[name] = dict(
+                line = self.container.appendLineSublayer(
+                    startPoint=(-border*2, value),
+                    endPoint=((self.glyph.width + border)*2, value),
+                    strokeWidth=0.5,
+                    strokeColor=metricsStrokeColor,
+                ),
+                text = self.container.appendTextLineSublayer(
+                    # position=(n[0], value),
+                    position=(self.glyph.width, value),
+                    offset=(20, 4),
+                    size=(20, 20),
+                    weight="medium",
+                    pointSize=fontsize,
+                    text=displayName,
+                    fillColor=metricsTextColor,
+                    horizontalAlignment="left",
+                    verticalAlignment="bottom",
+                )
+            )
+
+    def customMetricsDataDidChange(self, info):
+        # print("customMetricsDataDidChange ///////////////////")
+        # print(info)
+        self.drawCustomMetrics(self.glyph.font)
+
+    glyphEditorGlyphDidChangeContoursDelay = 0.01
+
+    def glyphEditorGlyphDidChangeContours(self, info):
+        if self.glyph.leftMargin != self.leftMargin or self.glyph.rightMargin != self.rightMargin:
+            newTitleX = self.glyph.width
+            newLineEndX = (self.glyph.width + border)*2
+            for name, metrics in self.merzMetrics.items():
+                y = metrics["line"].getEndPoint()[1]
+                metrics["line"].setEndPoint((newLineEndX, y))
+                y = metrics["text"].getPosition()[1]
+                metrics["text"].setPosition((newTitleX, y))
+        else:
+            self.leftMargin = self.glyph.leftMargin
+            self.rightMargin = self.glyph.rightMargin
+
+
+def subscriberInfoExtractor(subscriber, info):
+    info["old"] = None
+    info["new"] = None
+    for lowLevelEvent in info["lowLevelEvents"]:
+        info["old"] = lowLevelEvent["old"]
+        info["new"] = lowLevelEvent["new"]
+
+
+if __name__ == "__main__":
+    registerSubscriberEvent(
+        subscriberEventName=f"{KEY}.changed",
+        methodName="customMetricsDataDidChange",
+        lowLevelEventNames=[f"{KEY}.changed"],
+        eventInfoExtractionFunction=subscriberInfoExtractor,
+        dispatcher="roboFont",
+        documentation="Send when the custom metrics lib key did change parameters.",
+        delay=0,
+        debug=True,
+    )
+    registerGlyphEditorSubscriber(CustomMetricsGuidesSubscriber)
---
+++ source/lib/settingsWindow.py
@@ -0,0 +1,113 @@
+import ezui
+from mojo.events import postEvent
+from mojo.roboFont import CurrentFont
+
+KEY = "com.adbac.customMetricsGuides"
+
+
+class SettingsWindowController(ezui.WindowController):
+
+    def build(self):
+        content = """
+        |--------------| @table
+        |              |
+        |--------------|
+        > (+-)    @addRemoveButton
+        """
+
+        self.f = CurrentFont()
+
+        tableItems=[
+            dict(
+                name=name,
+                value=value,
+            ) for name, value in self.f.lib.get(KEY, {}).items()
+        ]
+
+        descriptionData=dict(
+            table=dict(
+                width=300,
+                height=200,
+                items=tableItems,
+                drawFocusRing=False,
+                allowsMultipleSelection=False,
+                allowsEmptySelection=False,
+                columnDescriptions=[
+                    dict(
+                        identifier="name",
+                        title="Name",
+                        # width=35,
+                        editable=True
+                    ),
+                    dict(
+                        identifier="value",
+                        title="Y Value",
+                        # width=50,
+                        cellDescription=dict(
+                            valueType="integer"
+                        ),
+                        editable=True
+                    ),
+                ]
+            )
+        )
+
+        self.w = ezui.EZWindow(
+            title="Custom Metrics Guides - Settings",
+            content=content,
+            controller=self,
+            descriptionData=descriptionData,
+        )
+
+    def started(self):
+        self.w.open()
+
+    def addRemoveButtonAddCallback(self, sender):
+        table = self.w.getItem("table")
+        data = self.f.lib.get(KEY, {})
+        name = "New Guide"
+        baseName = name
+        counter = 1
+        while name in data:
+            name = f"{baseName} {counter}"
+            counter += 1
+        item = table.makeItem(
+            name=name,
+            value=0,
+        )
+        table.appendItems([item])
+        self.saveData()
+        postEvent(f"{KEY}.changed", old=None, new=(name, 0))
+
+    def addRemoveButtonRemoveCallback(self, sender):
+        table = self.w.getItem("table")
+        removedItem = table.getSelectedItems()[0]
+        removedData = (removedItem["name"], removedItem["value"])
+        table.removeSelectedItems()
+        self.saveData()
+        postEvent(f"{KEY}.changed", old=removedData, new=None)
+
+    def tableEditCallback(self, sender):
+        table = self.w.getItem("table")
+        data = self.f.lib[KEY]
+        editedItem = sender.getSelectedItems()[0]
+        editedData = (editedItem["name"], editedItem["value"])
+        editedIndex = sender.getSelectedIndexes()[0]
+        uneditedData = list(data.items())[editedIndex]
+        for i, name in enumerate(data.keys()):
+            if i == editedIndex:
+                continue
+            if name == editedItem["name"]:
+                table.setitemValue(editedIndex, "name", uneditedData[0])
+                break
+        self.saveData()
+        postEvent(f"{KEY}.changed", old=uneditedData, new=editedData)
+
+    def saveData(self):
+        table = self.w.getItem("table")
+        data = {item["name"]: item["value"] for item in table.get()}
+        self.f.lib[KEY] = data
+
+
+if CurrentFont() and __name__ == "__main__":
+    SettingsWindowController()
